3274559ec1d0e70ee263fb830d21707df38b98a9
GodieG-05 <152017568+GodieG-05@users.noreply.github.com>
2024-08-13 06:55:03 -0500

    feat: :sparkles: project started

---
+++ src/test/java/com/campuslands/examenfinal/ExamenfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campuslands.examenfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
